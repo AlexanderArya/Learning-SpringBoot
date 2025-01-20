eda9a06b0736279f6c49eea0e792a0d53f8b29cb
Andrean Bimo <bimowibowo4846@gmail.com>
2025-01-20 11:21:34 +0700

    SpringBoot Lanjutan

---
+++ mpservice3/src/main/java/com/manpro/mpservice3/Mpservice3Application.java
@@ -0,0 +1,13 @@
+package com.manpro.mpservice3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mpservice3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mpservice3Application.class, args);
+	}
+
+}
